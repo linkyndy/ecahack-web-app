baa2effabd89dfc968c2f7f76e0c6d158a4a00b8
linkyndy <linkyndy@gmail.com>
2013-12-01 16:04:30 +0200

    Added create_admin and recreate_db to manager

--- manage.py
+++ manage.py
@@ -1,6 +1,8 @@
-from flask.ext.script import Manager, Server, prompt_bool
+from flask.ext.script import Manager, Server, prompt_bool, prompt, prompt_pass
 
 from ecahack import app, db
+from ecahack.users.models import User
+from ecahack.users.constants import ADMIN
 
 
 manager = Manager(app)
@@ -14,11 +16,33 @@ def create_db():
     db.create_all()
 
 @manager.command
+def create_admin():
+    """Creates an admin for the app"""
+
+    username = prompt('Enter admin username', default='admin')
+    password = prompt_pass('Enter admin password', default='admin')
+
+    user = User(rfid='00000000000000',
+                username=username,
+                password=password,
+                role=ADMIN)
+    db.session.add(user)
+    db.session.commit()
+
+@manager.command
 def drop_db():
     """Drops database tables"""
 
     if prompt_bool("Are you sure you want to lose all your data?"):
         db.drop_all()
+
+@manager.command
+def recreate_db():
+    """Drops database tables, creates them and prompts for an admin"""
+
+    drop_db()
+    create_db()
+    create_admin()
 
 @manager.command
 def test():
